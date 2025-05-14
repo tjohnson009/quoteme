e68d6b73c2a26ee7f06c1c883b1cb9258362961c
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2025-05-14 17:14:01 -0400

    Create post route for quotes

--- backend/v1/api/quotes/controller.ts
+++ backend/v1/api/quotes/controller.ts
@@ -1,16 +1,41 @@
 // controller = handle the request and response; fetches data from the database and send it back
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
+import Quote from '../../models/quote.model'; // Import the Quote interface 
 
-function getQuotes(req: Request, res: Response, next?: NextFunction) : void {
+function getQuotes(req: Request, res: Response) : void {
     const sampleQuotes = [
         { id: 1, quote: "The only way to do great work is to love what you do." },
-        { id: 2, quote: "Success is not the key to happiness. Happiness is the key to success." },
+        { id: 2, quote: "WORK is not the key to happiness. Happiness is the key to success." },
         { id: 3, quote: "Believe you can and you're halfway there." },
         { id: 4, quote: "The future belongs to those who believe in the beauty of their dreams." },
         { id: 5, quote: "You miss 100% of the shots you don't take." }
     ];
 
     res.json(sampleQuotes);
+}
+
+function createQuote(req: Request, res: Response) {
+if (!req.body.text) {
+    return res.status(400).json({error: 'Quote text is not present.'}); 
+}
+
+if (!req.body.author) {
+    req.body.author = 'Unknown'; // default  
+}
+
+    const newQuote: Quote = {
+        id: Math.floor(Math.random() * 1000), // random id
+        createdAt: new Date(), // current date  
+        author: '', 
+        text: 'If you cannot be happy, be sad.',
+        tags: ['sad', 'happy']  
+    };
+
+    // sampleQuotes.push(newQuote); 
+    res.status(200).json(newQuote);  
+
+
+
 }
 
 export default getQuotes;
--- backend/v1/api/quotes/routes.ts
+++ backend/v1/api/quotes/routes.ts
@@ -1,9 +1,12 @@
 // Routes = URL + HTTP verb + which function to run
 import express from 'express';  
 import getQuotes from './controller'; 
+import createQuote from './controller';  
 
 const router = express.Router(); 
 
 router.get('/', getQuotes); // when the user hits /v1/api/quotes, run the getQuotes function 
+
+router.post('/', createQuote); // when the user hits /v1/api/quotes, run the createQuote function 
 
 export default router; 
