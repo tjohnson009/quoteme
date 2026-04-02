344cef05dcd65e2a95e581796b6040982026096a
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2026-04-01 21:11:43 -0400

    Remove console.log

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -4,7 +4,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
-  console.log(req.body); 
   const { email, password } = req.body;
   
   try {
