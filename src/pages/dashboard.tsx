411f6d074aabac76affd0d9474cdcb4fea5a4a5c
Tim Johnson <timjohnson0914@gmail.com>
2026-04-23 15:06:56 -0400

    Update dashboard to show user quotes upon login

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,23 +1,51 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import {logout} from '@/services/auth'; // proxy logout function
-import { log } from 'console';
+import { Quote, getQuotes, deleteQuote } from '@/services/quotes';
+import QuoteCard from '@/components/QuoteCard'
 import Button from '@/components/Button';
 import Navbar from '@/components/Navbar'; 
 import Footer from '@/components/Footer';
 
 export default function Dashboard() {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null) 
   const [authorized, setAuthorized] = useState(false);
   const router = useRouter();
+
+  const handleDelete = async (id: number) => {
+    // delete the quote from the database
+    try {
+      await deleteQuote(id);
+      setQuotes((prevQuotes) => { return prevQuotes.filter(quote => quote.id !== id)})
+      // if successful, then renove the quote from the UI
+    } catch(err) {
+      setError("Failed to delete quote."); 
+      console.error("Something went wrong while deleting your quote:", `${err}`); 
+    }
+  }
 
   useEffect(() => { 
     const token = localStorage.getItem('token'); // ← use a better method later, for now quick
     if (!token) {
       router.push('/');
     } else {
-      setAuthorized(true);
-      setLoading(false);
+      const fetchQuotes = async () =>  {
+        try {
+          setAuthorized(true);
+          setLoading(false);
+          const data = await getQuotes();
+          setQuotes(data)
+        } catch (err) {
+          setError("Failed to load quotes from database.");
+          console.error(err)
+        } finally {
+          setLoading(false)
+        }
+      };
+
+      fetchQuotes()
     }
   }, []);
   
@@ -28,8 +56,20 @@ export default function Dashboard() {
   return ( 
     <>
     <main className="min-h-screen">
-      <p className="my-2 w-full mx-auto text-sm text-center text-gray-600">You do not have any quotes saved {user ? user.email : 'Guest'}!</p>
-    </main>
+
+      { loading && <p>Loading your quotes!</p> }
+      { error && <p className="error">There was a problem when loading your quotes: {error}</p> }
+      { !loading && !error && quotes.length === 0 && <p className="my-2 w-full mx-auto text-sm text-center text-gray-600">You do not have any quotes saved yet {user ? user.email : 'Guest'}. Start saving quotes now to see them here!</p> }
+      { quotes.map(quote => (
+        <QuoteCard 
+          key={quote.id} 
+          quote={quote} 
+          onDelete={() => {
+            handleDelete(quote.id);
+          }}
+           />
+      ))} 
+          </main>
     </>
   );
 }
