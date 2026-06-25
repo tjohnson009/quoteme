8d3a307ea20a5dd84a57d3b69b5834f4ba81a2ae
Tim Johnson <timjohnson0914@gmail.com>
2026-06-25 19:51:24 -0400

    Formatting with prettier changes

--- src/components/NewQuoteForm.tsx
+++ src/components/NewQuoteForm.tsx
@@ -6,10 +6,10 @@ interface NewQuoteFormProps {
 }
 
 export default function NewQuoteForm(props: NewQuoteFormProps) {
-    const [text, setText] = useState(''); 
-    const [author, setAuthor] = useState(''); 
-    const [tags, setTags] = useState(''); 
-    const [notes, setNotes] = useState(''); 
+    const [text, setText] = useState('');
+    const [author, setAuthor] = useState('');
+    const [tags, setTags] = useState('');
+    const [notes, setNotes] = useState('');
     const [submitting, setSubmitting] = useState(false);
     const [error, setError] = useState('');
 
@@ -17,7 +17,7 @@ export default function NewQuoteForm(props: NewQuoteFormProps) {
         e.preventDefault();
         try {
             if (text.trim() !== '') {
-                setSubmitting(true); 
+                setSubmitting(true);
                 const quote = await createQuote(text.trim(), author, tags.split(',').map(t => t.trim()), notes)
                 props.onQuoteCreated(quote)
                 setText('')
@@ -27,12 +27,12 @@ export default function NewQuoteForm(props: NewQuoteFormProps) {
             } else {
                 console.error("Text must be included in the quote to save it!");
             }
-        } catch(err) {
-            setError("Failed to create quote."); 
+        } catch (err) {
+            setError("Failed to create quote.");
             console.error(error, "Something went wrong when trying to create that quote. Try again in a minute...");
             console.error(err)
         } finally {
-            setSubmitting(false); 
+            setSubmitting(false);
         }
     }
 
@@ -40,17 +40,17 @@ export default function NewQuoteForm(props: NewQuoteFormProps) {
         <form onSubmit={handleSubmit} className="border border-amber-400 flex flex-col min-h-1/2">
             {error && <p className="text-red-500">{error}</p>}
             <label htmlFor="text">Quote: </label>
-            <input type="text" id="text" value={text} onChange={(e) => setText(e.target.value)} required/>
+            <input type="text" id="text" value={text} onChange={(e) => setText(e.target.value)} required />
 
             <label htmlFor="author">Author: </label>
-            <input type="text" id="author" value={author} onChange={(e) => setAuthor(e.target.value)}/>
+            <input type="text" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
 
             <label htmlFor="tags">Tags (comma separated)</label>
-            <input type="text" id="tags" value={tags} onChange={(e) => setTags(e.target.value)}/>
+            <input type="text" id="tags" value={tags} onChange={(e) => setTags(e.target.value)} />
 
             <label htmlFor="notes">Notes</label>
-            <input type="text" id="notes" value={notes} onChange={(e) => setNotes(e.target.value)}/>
+            <input type="text" id="notes" value={notes} onChange={(e) => setNotes(e.target.value)} />
             <button type="submit" disabled={submitting}>Save Quote</button>
-    </form>
+        </form>
     )
 }
