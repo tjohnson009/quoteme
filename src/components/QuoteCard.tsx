47ae3b78bbb561c9761549faa59e615f4091d269
Tim Johnson <timjohnson0914@gmail.com>
2026-06-25 16:52:11 -0400

    Modify and sharpen QuoteCard component

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -2,14 +2,14 @@ export default function QuoteCard({ quote, onDelete }: {
     quote: { id: number, text: string, author: string, tags?: string[], notes?: string }, 
     onDelete: () => void }) {
   return (
-    <div className="border p-4 rounded shadow-sm bg-white">
-      <p className="text-lg font-semibold text-blue-700">“{quote.text}”</p>
-      <p className="text-sm text-gray-600 italic">— {quote.author}</p>
+    <div className="border p-8 rounded-xl shadow-sm bg-background-secondary hover:border-transparent">
+      <p className="text-lg font-semibold fg-primary">“{quote.text}”</p>
+      <p className="text-sm fg-primary italic">{quote.author !== "Unknown" && (quote.author)}</p>
       <div>
         <p className="text-sm text-gray-600">{quote.notes ? `Notes` : ''}</p>
         <div>{quote.notes}</div>
       </div>
-      <button onClick={onDelete} className="text-red-500 text-sm mt-2">Delete</button>
+      <button onClick={onDelete} className="text-error text-sm mt-2">Delete</button>
     </div>
   );
 }
