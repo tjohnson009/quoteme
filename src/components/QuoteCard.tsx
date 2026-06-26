7934e78860694a05fcef0127c7b247ee08e4eee4
Tim Johnson <timjohnson0914@gmail.com>
2026-06-25 20:59:28 -0400

    Start to build out QuoteCard structure

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -2,7 +2,7 @@ export default function QuoteCard({ quote, onDelete }: {
     quote: { id: number, text: string, author: string, tags?: string[], notes?: string }, 
     onDelete: () => void }) {
   return (
-    <div className="border p-8 rounded-xl shadow-sm bg-background-secondary hover:border-transparent">
+    <div className="border p-8 min-h-50 rounded-xl shadow-sm bg-background-secondary hover:border-transparent">
       <p className="text-lg font-semibold fg-primary">“{quote.text}”</p>
       <p className="text-sm fg-primary italic">{quote.author !== "Unknown" && (quote.author)}</p>
       <div>
